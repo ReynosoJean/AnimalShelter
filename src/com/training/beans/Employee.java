9e9004d2aea01f042cd94440be6c19cc54ac76c7
jean.reynoso <jean.reynoso@pointwest.com>
2017-11-21 19:43:19 +0800

    add employee bean

---
+++ src/com/training/beans/Employee.java
@@ -0,0 +1,16 @@
+public class Employee{
+
+	private int employeeID;
+
+public employee(){
+	this.employeeId = 1;
+}
+
+public void setEmployeeId(int employeeId){
+	this.employeeId = employeeId;
+}
+
+public int getEmployeeId(){
+return employeeId;
+}
+}
